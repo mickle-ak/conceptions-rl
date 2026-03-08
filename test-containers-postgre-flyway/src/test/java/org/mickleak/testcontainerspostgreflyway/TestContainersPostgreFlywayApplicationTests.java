4e7cc56c0b47b079ee39011f9b2c34ef2413a9cc
kiselev <Mikhail.Kiselev@adesso.de>
2026-03-08 17:10:04 +0100

    create project simular to real

---
+++ test-containers-postgre-flyway/src/test/java/org/mickleak/testcontainerspostgreflyway/TestContainersPostgreFlywayApplicationTests.java
@@ -0,0 +1,11 @@
+package org.mickleak.testcontainerspostgreflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainersPostgreFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {}  // NOSONAR
+}
